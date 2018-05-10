f7ab1418e1ad9caa5979b24828ead48adf11573c
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-05-09 22:44:36 -0500

    Se creo la clase MainWindow

--- src/persistence/FileManager.java
+++ src/persistence/FileManager.java
@@ -10,9 +10,9 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
+//import java.io.PrintWriter;
+//import java.net.InetSocketAddress;
+//import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
@@ -26,7 +26,7 @@ import javax.imageio.ImageIO;
 
 public class FileManager {
 
-		public static final String URL_PAGE = "http://wallpaperswide.com/search.html?q=";
+	public static final String URL_PAGE = "http://wallpaperswide.com/search.html?q=";
 	public static final String FOLDER = "src/files";
 	public static final File FILE = new File("src/files/file.txt");
 	public static final String READ_FILE = "src/files/file.txt";
@@ -34,20 +34,20 @@ public class FileManager {
 
 	private BufferedReader bufferedReader;
 
-//	public void readFile(String search) throws IOException {
-//		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.16.0.73", 8080));
-//		URL url = new URL(URL_PAGE + search); 
-//		URLConnection connection = url.openConnection(proxy);
-//		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-//		BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
-//		int ch;
-//		PrintWriter printWriter = new PrintWriter(FILE);
-//		while ((ch = in.read()) != -1) {
-//			printWriter.append((char) ch);
-//		}
-//		printWriter.close();
-//	}
-	
+	/*public void downloadFileTwo(String image) throws IOException {
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("172.16.0.73", 8080));
+		URL url = new URL(URL_PAGE + image); 
+		URLConnection connection = url.openConnection(proxy);
+		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+		BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+		int ch;
+		PrintWriter printWriter = new PrintWriter(FILE);
+		while ((ch = in.read()) != -1) {
+			printWriter.append((char) ch);
+		}
+		printWriter.close();
+	}*/
+
 	public static void downloadFile(String image) throws IOException {
 		URLConnection website = new URL(URL_PAGE + image).openConnection();
 		website.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
@@ -70,7 +70,6 @@ public class FileManager {
 
 		while((line=bufferedReader.readLine())!=null) {
 			count++;
-			//			System.out.println(count + "-" + line);
 			Pattern r = Pattern.compile(pattern);
 			Matcher m = r.matcher(line);
 			if (m.find()) {
@@ -80,7 +79,6 @@ public class FileManager {
 				downloadsImages(imageList);
 			}
 		}
-		//		System.out.println(imageList);
 		return imageList;
 	}
 
@@ -107,45 +105,39 @@ public class FileManager {
 			fos.close();
 		}
 	}
-	
+
 	public void addImageFilter() throws IOException {
 		File[] imgFiles = new File("images/").listFiles();
 		for (File file : imgFiles) {
 			addFilterToImage(file, ImageIO.read(file));
 		}
 	}
-	
-	private static void addFilterToImage(File file, BufferedImage bufferedImage) throws IOException {
+
+	public void addFilterToImage(File file, BufferedImage bufferedImage) throws IOException {
 		for (int i = 0; i < bufferedImage.getWidth(); i++) {
 			for (int j = 0; j < bufferedImage.getHeight(); j++) {
 				Color c1=new Color(bufferedImage.getRGB(i, j));
-                int med=(c1.getRed()+c1.getGreen()+c1.getBlue())/3;
-                bufferedImage.setRGB(i, j, new Color(med,med,med).getRGB());
+				int med=(c1.getRed()+c1.getGreen()+c1.getBlue())/3;
+				bufferedImage.setRGB(i, j, new Color(med,med,med).getRGB());
 			}
 		}
 		ImageIO.write(bufferedImage, "jpg", new File("imagesFilter/" + file.getName()));
 	}
-	
+
 
 	public static void main(String[] args) {
 		FileManager fileManager = new FileManager();
-		
+
 		try {
 			FileManager.downloadFile("love");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			fileManager.readPlayer();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-//		try {
-//			fileManager.addImageFilter();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
 	}
 }
